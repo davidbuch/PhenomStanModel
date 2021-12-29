80c3a0963662e18c81bff3694c754cff04714562
David Buch <davidbuch42@gmail.com>
2021-12-29 18:52:54 -0500

    First Commit

---
+++ R/PhenomStanModel-package.R
@@ -0,0 +1,16 @@
+#' The 'PhenomStanModel' package.
+#'
+#' @description A minimal package containing just one object - a compiled Stan model to be used in the Phenom Shiny Web App.
+#'
+#' @docType package
+#' @name PhenomStanModel-package
+#' @aliases PhenomStanModel
+#' @useDynLib PhenomStanModel, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (2021). RStan: the R interface to Stan. R package version 2.21.3. https://mc-stan.org
+#'
+NULL
